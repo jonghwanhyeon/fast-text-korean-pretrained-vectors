b38dd725fc57133349e778943a7f423116ff2d9c
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-12-03 22:28:21 +0900

    Initial commit

---
+++ notify.py
@@ -0,0 +1,45 @@
+# This script is for sending a notification via Pushover (https://pushover.net)
+# To use this, you must configure `authentication.json` first
+
+import os
+import sys
+import json
+import time
+
+import requests
+
+number_of_attempts = 5
+authentication_filename = 'authentication.json'
+
+if not os.path.exists(authentication_filename):
+    sys.exit('{} does not exist'.format(authentication_filename))
+
+if len(sys.argv) == 1:
+    sys.exit('Usage: python3 {} <message>'.format(sys.argv[0]))
+
+with open('authentication.json', 'r', encoding='utf-8') as input_file:
+    authentication = json.load(input_file)
+
+parameters = {
+    'user': authentication['user_key'],
+    'token': authentication['api_token'],
+    'message': ' '.join(sys.argv[1:])
+}
+
+success = False
+for attempt in range(0, number_of_attempts):
+    try:
+        response = requests.post('https://api.pushover.net/1/messages.json', data=parameters)
+        if response.status_code == 200:
+            success = True
+            break
+    except:
+        pass
+
+    if not success:
+        time.sleep(1)
+
+if success:
+    print('Notification sent')
+else:
+    sys.exit('An error occurred during sending notification')
