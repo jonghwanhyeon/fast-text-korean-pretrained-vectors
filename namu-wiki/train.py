c68a3c21b3156e4f7bae8d985823cf37c0d9cd3a
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-12-03 15:42:58 +0900

    Initial commit

---
+++ namu-wiki/train.py
@@ -0,0 +1,24 @@
+import logging
+import subprocess
+import fastText
+
+dataset_filename = 'namuwiki_20170327_dataset'
+model_filename = 'namuwiki_20170327_model'
+
+pushover = {
+    'token': '',
+    'user': '',
+}
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info('Training model...')
+model = fastText.train_unsupervised(
+    input=dataset_filename,
+    dim=300,
+    epoch=10
+)
+
+logger.info('Saving model...')
+model.save_model(model_filename)
